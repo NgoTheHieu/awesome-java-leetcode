8b5bf4f03dfd07ae6884958dcbbfa2de379826cd
root <root@DESKTOP-RVJN9AT.localdomain>
2020-11-08 19:24:21 +0700

    Adding easy and medium

--- src/com/blankj/easy/_0083/Solution.java
+++ src/com/blankj/easy/_0083/Solution.java
@@ -2,6 +2,18 @@ package com.blankj.easy._0083;
 
 import com.blankj.structure.ListNode;
 
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+// Example 1:
+
+// Input: 1->1->2
+// Output: 1->2
+// Example 2:
+
+// Input: 1->1->2->3->3
+// Output: 1->2->3
+
+
 /**
  * <pre>
  *     author: Blankj
--- src/com/blankj/easy/_0101/Solution.java
+++ src/com/blankj/easy/_0101/Solution.java
@@ -5,6 +5,28 @@ import com.blankj.structure.TreeNode;
 
 import java.util.LinkedList;
 
+// Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+// For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+//     1
+//    / \
+//   2   2
+//  / \ / \
+// 3  4 4  3
+ 
+
+// But the following [1,2,2,null,3,null,3] is not:
+
+//     1
+//    / \
+//   2   2
+//    \   \
+//    3    3
+ 
+
+// Follow up: Solve it both recursively and iteratively.
+
 /**
  * <pre>
  *     author: Blankj
